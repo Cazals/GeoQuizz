93079f94ac5778b1b25ab53580972086ce558ae3
Matthieu <matthieubalondrade@outlook.fr>
2018-01-08 20:45:04 +0100

    Added player attributes and ban action

--- front/players/playerAdmin.php
+++ front/players/playerAdmin.php
@@ -11,30 +11,6 @@
     <!-- Start your content here !! you dumb -->
     <main class="mdl-layout__content">
         <div class="mdl-grid" id="ma_grille_de_joueurs">
-
-            <div class="demo-card-square mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col" id="maDiv"
-                 style="display: none">
-                <div class="mdl-card__title mdl-card--expand">
-                    <h2 class="mdl-card__title-text">Pseudo</h2>
-                </div>
-                <div class="mdl-card__supporting-text">
-                    Mon test Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Aenan convallis.
-                </div>
-                <div class="mdl-card__actions mdl-card--border">
-                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" id="monBoutton2">
-                        Modify
-                    </a>
-                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect bouton_supprimer"
-                       id_joueur="4">
-                        Bann
-                    </a>
-                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect bouton_supprimer"
-                       id_joueur="4">
-                        Delete
-                    </a>
-                </div>
-            </div>
         </div>
         <button id="b_add"
                 class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
@@ -50,15 +26,15 @@
             <label class="mdl-textfield__label" for="c_pseudo">Login</label>
         </div>
         <div class="mdl-textfield mdl-js-textfield">
-            <input class="mdl-textfield__input" type="email"  id="c_email"/>
+            <input class="mdl-textfield__input" type="email" id="c_email"/>
             <label class="mdl-textfield__label" for="c_email">Email</label>
         </div>
         <div class="mdl-textfield mdl-js-textfield">
-            <input class="mdl-textfield__input" type="text"  id="c_firstName"/>
+            <input class="mdl-textfield__input" type="text" id="c_firstName"/>
             <label class="mdl-textfield__label" for="c_firstName">FirstName</label>
         </div>
         <div class="mdl-textfield mdl-js-textfield">
-            <input class="mdl-textfield__input" type="text"  id="c_lastName"/>
+            <input class="mdl-textfield__input" type="text" id="c_lastName"/>
             <label class="mdl-textfield__label" for="c_lastName">LastName</label>
         </div>
         <div class="mdl-textfield mdl-js-textfield">
@@ -69,15 +45,14 @@
             <input class="mdl-textfield__input" type="password" id="c_password"/>
             <label class="mdl-textfield__label" for="c_password">Password</label>
         </div>
-            <input class="mdl-textfield__input" type="file" id="c_image">
-        </div>
-        <div class="mdl-card__actions mdl-card--border">
-            <a id="bouton_enregistrer_joueur">
-                <button class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-                    Enregistrer
-                </button>
-            </a>
-        </div>
+        <input class="mdl-textfield__input" type="file" id="c_image">
+    </div>
+    <div class="mdl-card__actions mdl-card--border">
+        <a id="bouton_enregistrer_joueur">
+            <button class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                Enregistrer
+            </button>
+        </a>
     </div>
 </div>
 
